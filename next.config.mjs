48ab0993bccc8c21609e8d5e977cace0bc0af2b1
Maksym Symonov <ssmaksss33@gmail.com>
2024-08-07 12:33:49 +0300

    three commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    ignoreDevErrors: true,
+    ignoreLintErrors: true,
+    ignoreBuildErrors: true,
+  }
+};
 
 export default nextConfig;
